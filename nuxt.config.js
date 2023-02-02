d859cf6afda331585665560db9138adccda65059
joy hong <remonkp@gmail.com>
2023-02-02 11:55:59 +0900

    배포 테스트

--- nuxt.config.js
+++ nuxt.config.js
@@ -83,7 +83,6 @@ export default {
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
     baseURL: development ? "http://localhost:8080" : "https://joyhong9102.netlify.app",
-    https: true
   },
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {},
